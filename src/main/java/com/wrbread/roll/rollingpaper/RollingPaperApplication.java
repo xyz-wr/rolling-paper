b6ac05820b29a954519da5ca5efe83f09ae790e8
xyz-wr <pponi13468@naver.com>
2024-01-19 15:12:16 +0900

    프로젝트 생성

---
+++ src/main/java/com/wrbread/roll/rollingpaper/RollingPaperApplication.java
@@ -0,0 +1,13 @@
+package com.wrbread.roll.rollingpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingPaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RollingPaperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wrbread/roll/rollingpaper/RollingPaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrbread.roll.rollingpaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollingPaperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

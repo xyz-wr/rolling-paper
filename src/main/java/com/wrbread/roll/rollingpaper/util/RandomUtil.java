cddc267a7471a682855612a844ab958106cb285f
xyz-wr <pponi13468@naver.com>
2024-03-06 14:54:24 +0900

    ThreadLocalRandom 사용해보기

--- src/main/java/com/wrbread/roll/rollingpaper/util/RandomUtil.java
+++ src/main/java/com/wrbread/roll/rollingpaper/util/RandomUtil.java
@@ -2,18 +2,18 @@ package com.wrbread.roll.rollingpaper.util;
 
 import org.springframework.stereotype.Component;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 public class RandomUtil {
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
     public static String generateRandomString() {
         int length = 6; // 원하는 문자열 길이
-        Random random = new Random();
         StringBuilder stringBuilder = new StringBuilder(length);
 
         for (int i = 0; i < length; i++) {
-            int randomIndex = random.nextInt(CHARACTERS.length());
+            int randomIndex = ThreadLocalRandom.current().nextInt(CHARACTERS.length());
             char randomChar = CHARACTERS.charAt(randomIndex);
             stringBuilder.append(randomChar);
         }
